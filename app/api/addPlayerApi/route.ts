99e2127aa10b71d3b1fe5636ba163f9cdbf349a8
Mayank Dwivedi <mayankanildwivedi@gmail.com>
2024-03-06 09:17:55 +0530

    Added route for adding player

---
+++ app/api/addPlayerApi/route.ts
@@ -0,0 +1,160 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import moment from "moment";
+
+const getPlayersByTeam = (
+  matchId: string,
+  teamId: string
+): { [team: string]: string[] } => {
+  const filePath = path.join(
+    process.cwd(),
+    "data",
+    matchId,
+    "matchScoreboardUpdated.json"
+  );
+
+  const additionalPlayersFilePath = path.join(
+    process.cwd(),
+    "data",
+    matchId,
+    "additionalPlayers.json"
+  );
+  let existingPlayers: any = [];
+  try {
+    if (fs.existsSync(additionalPlayersFilePath)) {
+      const additionalPlayersData = fs.readFileSync(
+        additionalPlayersFilePath,
+        "utf-8"
+      );
+      existingPlayers = JSON.parse(additionalPlayersData);
+    }
+  } catch (error) {
+	// offcourse we can log the error here, ignoring to save time
+  }
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, "utf-8");
+    const scoreboard = JSON.parse(fileData);
+
+    try {
+      const matchDetails = scoreboard.match_details;
+      // select team here which team
+      /**
+       * here we will check if the team id is equal to team_a_new_det.team_id then we will return team_a_new_det.players
+       * if else we will check if the team id is equal to team_b_new_det.team_id then we will return team_b_new_det.players
+       * else we will return null
+       */
+
+      let teamPlayersList = null; // because we are not sure about the team_id
+      if (matchDetails.team_a_new_det.team_id == teamId)
+        teamPlayersList = matchDetails.team_a_new_det?.players;
+      else if (matchDetails.team_b_new_det.team_id == teamId)
+        teamPlayersList = matchDetails.team_b_new_det?.players;
+      return { players: [...existingPlayers, ...teamPlayersList] };
+    } catch (error) {
+      return { players: [] };
+    }
+  }
+
+  return {};
+};
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const matchId = searchParams.get("matchId");
+  const teamId = searchParams.get("teamId");
+
+  if (matchId) {
+    const playersByTeam = getPlayersByTeam(matchId, teamId as string);
+    return NextResponse.json(playersByTeam);
+  } else {
+    return NextResponse.json({ error: "Invalid matchId" });
+  }
+}
+
+export async function POST(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const matchId = searchParams.get("matchId");
+  const teamId = searchParams.get("teamId");
+  const playerData = await req.json();
+  if (!matchId || !teamId) {
+    return NextResponse.json({ error: "Invalid matchId or teamId" });
+  }
+
+  // Read the existing match scoreboard file
+  const filePath = path.join(
+    process.cwd(),
+    "data",
+    matchId,
+    "matchScoreboard.json"
+  );
+  const playerListPath = path.join(
+    process.cwd(),
+    "data",
+    matchId,
+    "additionalPlayers.json"
+  );
+  if (fs.existsSync(filePath)) {
+    const fileData = fs.readFileSync(filePath, "utf-8");
+    const scoreboard = JSON.parse(fileData);
+
+    try {
+      const matchDetails = scoreboard.match_details;
+      let teamPlayersList = null;
+      let additionalPlayers = [];
+
+      // Check if additionalPlayers.json exists
+      if (fs.existsSync(playerListPath)) {
+        const additionalPlayersData = fs.readFileSync(playerListPath, "utf-8");
+        additionalPlayers = JSON.parse(additionalPlayersData);
+      }
+
+      if (matchDetails.team_a_new_det.team_id == teamId) {
+        teamPlayersList = matchDetails.team_a_new_det?.players;
+        let isPlayerPresent = [
+          ...teamPlayersList,
+          ...additionalPlayers,
+        ].findIndex((player: any) => {
+          return (
+            player.name.toLowerCase().trim() ==
+            playerData.name.toLowerCase().trim()
+          );
+        });
+        if (isPlayerPresent > -1) {
+          return NextResponse.json({ error: "Player already exists" });
+        }
+        const createdAt = moment().format("YYYY-MM-DD HH:mm:ss");
+        playerData["teamId"] = teamId;
+        playerData["createdAt"] = createdAt;
+        additionalPlayers.push(playerData);
+      } else if (matchDetails.team_b_new_det.team_id == teamId) {
+        teamPlayersList = matchDetails.team_b_new_det?.players;
+        let isPlayerPresent = [
+          ...teamPlayersList,
+          ...additionalPlayers,
+        ].findIndex((player: any) => {
+          return (
+            player.name.toLowerCase().trim() ==
+            playerData.name.toLowerCase().trim()
+          );
+        });
+        if (isPlayerPresent > -1) {
+          return NextResponse.json({ error: "Player already exists" });
+        }
+        additionalPlayers.push(playerData);
+      }
+
+      // Update the match scoreboard file with the new player data
+      fs.writeFileSync(
+        playerListPath,
+        JSON.stringify(additionalPlayers, null, 2)
+      );
+
+      return NextResponse.json({ success: true });
+    } catch (error) {
+      return NextResponse.json({ error: "Failed to add player" });
+    }
+  } else {
+    return NextResponse.json({ error: "Match scoreboard file not found" });
+  }
+}
